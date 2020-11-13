6681ed25a2f220302a9c6845633ff7aaba802f1a
alejobh <alejo0395@gmail.com>
2020-11-12 19:44:01 -0500

    Added ts type to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../scss/index.scss';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
 
 /* eslint-disable-next-line @typescript-eslint/naming-convention */
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
